4f107f5ddb6c413dfc5294adcc6fe0097708f9aa
Vinícius Führ de Borba <tecnologia@tgwsistemas.com.br>
2022-01-24 09:14:47 -0300

    SSR Authentication

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
+import { GetServerSideProps } from "next";
 import { FormEvent, useContext, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
+
+import { withSSRGuest } from "../utils/withSSRGuest";
+
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -35,3 +39,9 @@ export default function Home() {
     </form>
   );
 }
+
+export const getServerSideProps = withSSRGuest(async (ctx) => {
+  return {
+    props: {},
+  };
+});
---
+++ utils/withSSRAuth.ts
@@ -0,0 +1,40 @@
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from "next";
+import { destroyCookie, parseCookies } from "nookies";
+import { AuthTokenError } from "../services/errors/AuthTokenError";
+
+export function withSSRAuth<P>(fn: GetServerSideProps<P>) {
+  return async (
+    ctx: GetServerSidePropsContext
+  ): Promise<GetServerSidePropsResult<P>> => {
+    const cookies = parseCookies(ctx);
+
+    if (!cookies["authentication-ignite.token"]) {
+      return {
+        redirect: {
+          destination: "/",
+          permanent: false,
+        },
+      };
+    }
+
+    try {
+      return await fn(ctx);
+    } catch (error) {
+      if (error instanceof AuthTokenError) {
+        destroyCookie(ctx, "authentication-ignite.token");
+        destroyCookie(ctx, "authentication-ignite.refreshToken");
+
+        return {
+          redirect: {
+            destination: "/",
+            permanent: false,
+          },
+        };
+      }
+    }
+  };
+}
