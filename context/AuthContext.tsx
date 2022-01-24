c7da112223cc4f676bfc9bfab79b4ad21edb0aec
Vinícius Führ de Borba <tecnologia@tgwsistemas.com.br>
2022-01-24 11:42:47 -0300

    Permission Control

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -16,7 +16,8 @@ type SignInCredentials = {
 };
 
 type AuthContextData = {
-  signIn(credentials: SignInCredentials): Promise<void>;
+  signIn: (credentials: SignInCredentials) => Promise<void>;
+  signOut: () => void;
   isAuthenticated: boolean;
   user: User;
 };
@@ -27,9 +28,13 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData);
 
+let authChannel: BroadcastChannel;
+
 export function signOut() {
   destroyCookie(undefined, "authentication-ignite.token");
   destroyCookie(undefined, "authentication-ignite.refreshToken");
+
+  authChannel.postMessage("signOut");
 
   Router.push("/");
 }
@@ -37,6 +42,22 @@ export function signOut() {
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
+
+  useEffect(() => {
+    authChannel = new BroadcastChannel("signOut");
+
+    authChannel.onmessage = (message) => {
+      switch (message.data) {
+        case "signOut": {
+          signOut();
+          authChannel.close();
+          break;
+        }
+        default:
+          break;
+      }
+    };
+  }, []);
 
   useEffect(() => {
     const { "authentication-ignite.token": token } = parseCookies();
@@ -85,7 +106,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
   }
 
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+    <AuthContext.Provider value={{ signIn, signOut, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
   );
